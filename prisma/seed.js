8c10cf960894c78d739f8001dad651f8d13794bd
Miguel Alcaino <malcaino@siroe.cl>
2026-07-13 10:03:57 -0400

    fix(seed): pronobel captura SUB->unidadesCaja y EMB->unidadesPallet en los 3 formatos

--- prisma/seed.js
+++ prisma/seed.js
@@ -28,9 +28,9 @@ const LIBRERIA = [
   {
     nombre: 'Pronobel', slug: 'pronobel',
     config: { configs: [
-      { colSku: 'CODIGO',    colNombre: 'Texto breve material', colPrecio: 'FINAL NETO',        colMarca: 'Marca', colBarras: 'BARRAS' },
-      { colSku: 'Material',  colNombre: 'Texto breve material', colPrecio: 'CASTILLA Y ARAGON', colMarca: 'Marca', colBarras: 'BARRAS' },
-      { colSku: 'CODIGO',    colNombre: 'DESCRIPCION',          colPrecio: '$ NETO',             colBarras: 'BARRAS' },
+      { colSku: 'CODIGO',   colNombre: 'Texto breve material', colPrecio: 'FINAL NETO',        colMarca: 'Marca', colBarras: 'BARRAS', colUnidadesCaja: 'SUB', colUnidadesPallet: 'EMB' },
+      { colSku: 'Material', colNombre: 'Texto breve material', colPrecio: 'CASTILLA Y ARAGON', colMarca: 'Marca', colBarras: 'BARRAS', colUnidadesCaja: 'SUB', colUnidadesPallet: 'EMB' },
+      { colSku: 'CODIGO',   colNombre: 'DESCRIPCION',          colPrecio: '$ NETO',             colBarras: 'BARRAS', colUnidadesCaja: 'SUB', colUnidadesPallet: 'EMB' },
     ] },
   },
   {
